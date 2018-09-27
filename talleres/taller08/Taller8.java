78bf8eebbbce0d28e85bacc59226714a0bad9c03
julianalalindev <41337746+julianalalindev@users.noreply.github.com>
2018-09-26 23:32:03 -0500

    Update Taller8.java

--- talleres/taller08/Taller8.java
+++ talleres/taller08/Taller8.java
@@ -68,4 +68,60 @@ public class Taller8 {
         }
         return num;
     }
+    
+    //Punto 2
+    //Clase para crear los objetos tipo nevera
+    public class Nevera {
+        private int codigo;
+        private String descripcion;
+
+        public Nevera (int codigo, String descripcion) {
+            this.codigo=codigo;
+            this.descripcion=descripcion;
+        }
+
+        public int getCodigo () {
+            return codigo;
+        }
+
+        public String getDescripcion () {
+            return descripcion;
+        }
+    }
+    //Clase para crear los objetos tipo solicitud
+    public class Solicitud {
+        private int neverasSolicitadas;
+        private String tienda;
+
+        public Solicitud (int neverasSolicitadas, String tienda) {
+            this.neverasSolicitadas=neverasSolicitadas;
+            this.tienda=tienda;
+        }
+
+        public int getNeverasSolicitadas () {
+            return neverasSolicitadas;
+        }
+
+        public String getTienda () {
+            return tienda;
+        }
+    }
+    
+    public static void neveras (Stack<Nevera> neveras, Queue<Solicitud> solicitudes) {
+        while(solicitudes.peek()!=null) {
+            Solicitud nueva=solicitudes.poll();
+            int nev=nueva.getNeverasSolicitadas();
+            System.out.print(nueva.getTienda()+":");
+            while(neveras.size()!=0 && nev!=0) {
+                Nevera aux=neveras.pop();
+                System.out.print(" ("+aux.getCodigo()+" , "+aux.getDescripcion()+")");
+                nev--;
+            }
+            if(solicitudes.peek()!=null && neveras.size()==0) {
+                System.out.print("No hay más neveras disponibles");
+            }
+            System.out.println();
+        }
+    }
+        
 }
