d2d30bd5ebe4018a84330fee53a96130856171d1
iurregog <41337834+iurregog@users.noreply.github.com>
2018-10-07 15:47:38 -0500

    Add files via upload

---
+++ laboratorios/lab03/codigo/Punto1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/**
+ * Write a description of class Punto1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Punto1
+{
+    //1.1  with LinkedList
+    public static void SmartInsert(LinkedList one, int data){
+        if(one.contains(data)){
+        }else{
+            one.add(data);
+        }
+
+    }
+    //1.1 with ArrayList
+    public static void SmartInsert(ArrayList one, int data){
+        if(one.contains(data)){
+        }else{
+            one.add(data);
+        }
+
+    }
+
+   
+}
