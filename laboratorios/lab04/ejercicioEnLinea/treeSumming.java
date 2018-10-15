5bf6204e939f7cd0b3c306728d2c4857562b17a0
iurregog <41337834+iurregog@users.noreply.github.com>
2018-10-15 12:44:44 -0500

    Add files via upload

---
+++ laboratorios/lab04/ejercicioEnLinea/treeSumming.java
@@ -0,0 +1,21 @@
+
+/**
+ * Excercise 2.2
+ *
+ * @author Mauricio Toro, Juliana Lalinde e Isabel Urrego
+ * @version 15/10/2018
+ */
+public class treeSumming
+{
+    public static boolean treeSumming(Node a, int sum) {
+        if (a == null) {
+            return false; 
+        }else{
+            if (a.left == null && a.right == null) {
+                return sum == a.data; 
+            }else {
+                return treeSumming(a.left, sum- a.data)  || treeSumming(a.right, sum-a.data); 
+            }
+        }
+    }
+}
