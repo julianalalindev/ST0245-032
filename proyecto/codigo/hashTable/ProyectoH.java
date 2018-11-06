43bb1919246c2b72db1f12a1219498502583f3c5
iurregog <41337834+iurregog@users.noreply.github.com>
2018-11-06 01:29:59 -0500

    Add files via upload

--- proyecto/codigo/hashTable/ProyectoH.java
+++ proyecto/codigo/hashTable/ProyectoH.java
@@ -1,18 +1,21 @@
 package proyectoh;
 /**
- *
+ ** Algorithm to prevent collisions between robotic bees
+ * Data Structure: Hash Tables
+ * 
  * @author Juliana Lalinde and Isabel Urrego
  */
 public class ProyectoH {
-
+ 
+ 
    public static void main(String[] args) {
         int bees;
-        bees=1000000;
+        //bees=1000000;
         //bees=100000;
         //bees=10000;
         //bees=1000;
         //bees=100;
-        //bees=10;
+        bees=1000000;
         //bees=4;
         CollisionRisk ex = new CollisionRisk(bees);
     }
