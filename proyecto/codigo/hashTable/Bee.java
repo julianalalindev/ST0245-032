c2a15507bed2fca6dc23747f6890ac314fda78ae
iurregog <41337834+iurregog@users.noreply.github.com>
2018-11-06 01:28:19 -0500

    Add files via upload

--- proyecto/codigo/hashTable/Bee.java
+++ proyecto/codigo/hashTable/Bee.java
@@ -1,6 +1,6 @@
 package proyectoh;
 /**
- *
+ * Class bee for Hash Table
  * @author Juliana Lalinde and Isabel Urrego
  */
 public class Bee {
@@ -9,7 +9,13 @@ public class Bee {
     private final double y;
     private final double z;
     private final String coordinatesInDegrees;
-    
+    /**
+     *  Constructor of bees
+     * @param x coordinate of the bee in x (Meters)
+     * @param y coordinate of the bee in y (Meters)
+     * @param z coordinate of the bee in z (Meters)
+     * @param coordinatesInDegrees  coordinate of the bee in y and z (Degrees)
+     */
     public Bee(double x, double y, double z, String coordinatesInDegrees) {
         this.x=x;
         this.y=y;
@@ -17,18 +23,33 @@ public class Bee {
         this.coordinatesInDegrees=coordinatesInDegrees;
     }
     
+    /**
+     * Accesor
+     * @return a double with the coordinate in x of the bee
+     */
     public double getX () {
         return x;
     }
     
+    /**
+     * accesor
+     * @return a double with the coordinate in y of the bee
+     */
     public double getY() {
         return y;
     }
     
+    /**
+     * Accesor
+     * @return a double with the coordinate in z of the bee
+     */
     public double getZ() {
         return z;
     }
-        
+        /**
+         * Accesor
+         * @return a String with the coordinates in y and z in degrees
+         */
     public String getCoordinatesInDegrees() {
         return coordinatesInDegrees;
     }
