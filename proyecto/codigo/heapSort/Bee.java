fdeb761256cd18ec5e8046e717164ecf26e7a659
iurregog <41337834+iurregog@users.noreply.github.com>
2018-11-06 01:48:06 -0500

    Add files via upload

--- proyecto/codigo/heapSort/Bee.java
+++ proyecto/codigo/heapSort/Bee.java
@@ -1,6 +1,6 @@
 package proyecto;
 /**
- *
+ * Class bee 
  * @author Juliana Lalinde and Isabel Urrego
  */
 public class Bee {
@@ -12,6 +12,13 @@ public class Bee {
     private double compValue;
     private boolean collides=false;
     
+    /**
+     * Constructor of object bee
+     * @param x double that has the coordinate in x of the bee (meters)
+     * @param y double that has the coordinate in y of the bee (meters)
+     * @param z double that has the coordinate in z of the bee (meters)
+     * @param coordinatesInDegrees  String that has the coordinate in y and z of the bee (degrees)
+     */
     public Bee(double x, double y, double z, String coordinatesInDegrees) {
         this.x=x;
         this.y=y;
@@ -19,34 +26,66 @@ public class Bee {
         this.coordinatesInDegrees=coordinatesInDegrees;
     }
     
+    /**
+     * Method to access to the coordinate in x of the bee
+     * @return value of the coordinate in x
+     */
     public double getX () {
         return x;
     }
     
+    /**
+     * Method to access to the coordinate in y of the bee
+     * @return value of the coordinate in y
+     */
     public double getY() {
         return y;
     }
     
+    /**
+     * Method to access to the coordinate in z of the bee
+     * @return value of the coordinate in z
+     */
     public double getZ() {
         return z;
     }
     
+    /**
+     * Method to access to the comparation value 
+     * @return double with the comparation value
+     */
     public double getCompValue() {
         return compValue;
     }
     
+    /**
+     * Method to set the comparation value
+     * @param compValue double with the actual comparation value that is going to be changed
+     */
     public void setCompValue (double compValue) {
         this.compValue=compValue;
     }
     
+    /**
+     * Method to know if the bee already collides
+     * @return boolean true if the bee collides
+     */
     public boolean getCollides() {
         return collides;
     }
     
+    /**
+     * Method to set the collision state of the bee
+     * @param collides actual collision state 
+     */
     public void setCollides (boolean collides) {
         this.collides=collides;
     }
     
+    /**
+     * Method to get the coordinates in degrees in y and z of a bee
+     * @return String with the coordinates in y and z in degrees
+     */
     public String getCoordinatesInDegrees() {
         return coordinatesInDegrees;
     }
